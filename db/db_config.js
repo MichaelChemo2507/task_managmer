fedfd03c9f41e039c681b5179c0fb7e71a76ee9b
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-07-22 16:28:36 +0300

    Adding the db config - working with mysql2

---
+++ db/db_config.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2/promise');
+
+const HOST = process.env.HOST;
+const USER = process.env.USER;
+const PASSWORD = process.env.PASSWORD;
+const DATABASE = process.env.DATABASE;
+
+const pool = mysql.createPool({
+  host: HOST,
+  user: USER,
+  password: PASSWORD,
+  database: DATABASE,
+});
+
+module.exports = {
+  pool: pool,
+};
