9ea3289ac93ea17d7477f9cfb1e1c4d18bd73526
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-07-22 16:32:40 +0300

    Add the initials for the server - using express js.

---
+++ index.js
@@ -0,0 +1,22 @@
+// npm i express body-parser mysql2 ejs dotenv cookie-parser
+const port = 7777;
+const dotenv = require('dotenv');
+const express = require('express');
+var cookieParser = require('cookie-parser');
+
+
+const app = express();
+app.use(express.json());
+
+app.use(cookieParser());
+dotenv.config();
+const bodyParser = require('body-parser');
+const path = require('path');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.listen(port, () => {
+  console.log(`Now listening on http://localhost:${port}/courses/listPage`);
+});
