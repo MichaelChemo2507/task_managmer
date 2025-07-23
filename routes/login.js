844be6edc116f81bbbbad945f7ebc904ac2fb579
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-07-23 19:36:52 +0300

    Finish working on the login repository.

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
+const LoginController = require('../controllers/login');
 const router = express.Router();
  
 router.get('/');
-router.post('/');
+router.post('/',LoginController.authorizationProcess);
 
 module.exports = router;
