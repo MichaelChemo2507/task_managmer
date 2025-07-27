47e819de64be4fae1b4e601d1c02aa55d1aa588d
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-07-27 13:42:39 +0300

    Added error message to the login page.

--- middleware/errorHandller.js
+++ middleware/errorHandller.js
@@ -3,7 +3,15 @@ const errorHandler = (error, req, res, next) => {
     if (error.code === 401) {
         res.status(error.code).send("client error: " + error.message + " - please check your registration."); // In the future it sapose to redirect to error page;
     } else if (error.code === 400 || error.code === 403 || error.code === 404) {
-        res.status(error.code).send("client error: " + error.message); // In the future it sapose to redirect to error page;
+        res.status(error.code).render('loginPage', {
+            data: {
+                inputs:req.body.userName,
+                errorMessage: error.message,
+                btnText: 'SUBMIT',
+                URL: 'http://localhost:7777/login/',
+                method: 'post',
+            },
+        })
     } else if (error.code === 500 || error.code === 501) {
         console.error("server Error: " + error.message + ": " + error.code);
         res.status(STATUS_CODES.INTERNAL_SERVER).send("Somthing whent wrong! please try again later.");
