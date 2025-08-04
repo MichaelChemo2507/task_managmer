5dc539a764851fac28248aabecbd6dcf07130985
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-08-04 13:54:12 +0300

    Finish working on the user controller and added the registration page.

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const LoginService = require('../services/users');
+const UsersService = require('../services/users');
 
 class LoginController {
     static async getLoginPage(req, res) {
@@ -10,14 +10,36 @@ class LoginController {
             },
         })
     }
+    static async getRegistrationPage(req, res) {
+
+    }
     static async authorizationProcess(req, res) {
         const oneDay = 24 * 60 * 60 * 1000
-        const accessToken = await LoginService.authorizationProcess(req.body);
+        const accessToken = await UsersService.authorizationProcess(req.body);
         res.cookie('token', accessToken, {
             httpOnly: true,
             maxAge: oneDay,
         });
-        return res.status(STATUS_CODES.OK);
+        return res.status(STATUS_CODES.OK).json({ success: true });
+    }
+    static async registrationProcess(req, res) {
+        
+    }
+    static async getAll(req, res) {
+        const users = await UsersService.getAll();
+        return res.status().json({ success: true, rows: users });
+    }
+    static async addUser(req, res) {
+        const insertId = await UsersService.addUser(req.body);
+        return res.status(process.env.CREATED).json({ success: true, insertId: insertId });
+    }
+    static async deleteUser(req, res) {
+        const affectedRows = await UsersService.deleteUser(parseInt(req.query.id));
+        return res.status(process.env.NO_CONTECT).json({ success: true, message: `rows by id ${id} are delete` });
+    }
+    static async updateUser(req, res) {
+        const affectedRows = await UsersService.updateUser(req.body, parseInt(req.params.id));
+        res.status(process.env.CREATED).json({ success: true, message: `rows by id ${id} are updated` });
     }
 }
 module.exports = LoginController;
--- middleware/errorHandller.js
+++ middleware/errorHandller.js
@@ -2,21 +2,23 @@ const errorHandler = (error, req, res, next) => {
     console.error(error);
     if (error.code === 401) {
         res.status(error.code).send("client error: " + error.message + " - please check your registration."); // In the future it sapose to redirect to error page;
-    } else if (error.code === 400 || error.code === 403 || error.code === 404) {
-        res.status(error.code).render('loginPage', {
-            data: {
-                inputs:req.body.userName,
-                errorMessage: error.message,
-                btnText: 'SUBMIT',
-                URL: 'http://localhost:7777/login/',
-                method: 'post',
-            },
-        })
+    } else if (error.code === 400) {
+        if (error.message === "Invalid user name!" || error.message === "Invalid password!") {
+            res.status(error.code).render('loginPage', {
+                data: {
+                    inputs: req.body.userName,
+                    errorMessage: error.message,
+                    btnText: 'SUBMIT',
+                    URL: 'http://localhost:7777/login/',
+                    method: 'post',
+                },
+            })
+        }
     } else if (error.code === 500 || error.code === 501) {
         console.error("server Error: " + error.message + ": " + error.code);
-        res.status(STATUS_CODES.INTERNAL_SERVER).send("Something went wrong! please try again later.");
+        res.status(STATUS_CODES.INTERNAL_SERVER).json({ success: false, message: "Something went wrong! please try again later." });
     } else {
-        res.status(STATUS_CODES.INTERNAL_SERVER).send("Something went wrong! please try again later.");
+        res.status(STATUS_CODES.INTERNAL_SERVER).json({ success: false, message: "Something went wrong! please try again later." });
     }
 
 };
