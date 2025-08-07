0511ff6e2d6b92c925d234a88d140c74a5313878
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-08-07 19:10:43 +0300

    Update the sql file.

--- db/task_manager.sql
+++ db/task_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2025 at 03:23 PM
+-- Generation Time: Aug 07, 2025 at 06:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -29,8 +29,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
-  `category_name` varchar(250) NOT NULL
+  `category_name` varchar(250) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category_name`, `user_id`) VALUES
+(46, 'SSR', 16),
+(49, 'React', 16),
+(51, 'JS', 16),
+(52, 'C#', 16),
+(53, 'Arduino', 16),
+(54, 'C++', 16),
+(55, 'C', 16),
+(56, 'Inovation', 16),
+(57, 'Server', 16),
+(58, 'Protocols', 16),
+(59, 'Front', 16),
+(60, 'צבע', 18);
 
 -- --------------------------------------------------------
 
@@ -41,11 +60,21 @@ CREATE TABLE `categories` (
 CREATE TABLE `tasks` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `description` text NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL,
   `date` date NOT NULL,
   `is_done` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `user_id`, `category_id`, `description`, `date`, `is_done`) VALUES
+(2, 16, 46, 'Building the first project.', '2025-08-29', 1),
+(3, 16, NULL, 'Algo exercises.', '2025-08-08', 1),
+(4, 16, 49, 'Finle project.', '2025-08-29', 2),
+(5, 18, 60, 'לצבוע את הבית', '2025-08-13', 1);
 
 -- --------------------------------------------------------
 
@@ -57,8 +86,17 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `user_name` varchar(250) NOT NULL,
   `email` varchar(250) NOT NULL,
-  `password` int(11) NOT NULL
+  `password` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `email`, `password`) VALUES
+(16, 'Micha_No_Logic', 'mnh8551@gmail.com', '0cc168013fd67d3a5419cdd4c36cbce5'),
+(17, 'Moshe_C', 'TRF8551@gmail.com', '0cc168013fd67d3a5419cdd4c36cbce5'),
+(18, 'DAVID', 'ndlkd@gmail.com', '0cc168013fd67d3a5419cdd4c36cbce5');
 
 --
 -- Indexes for dumped tables
@@ -68,21 +106,23 @@ CREATE TABLE `users` (
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `tasks`
 --
 ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `tasks_ibfk_1` (`category_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -92,30 +132,35 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
 -- Constraints for table `tasks`
 --
 ALTER TABLE `tasks`
-  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
-  ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
